12a09c84a630edc7b32088e9ac9e7caa53f0503a
Adam Eichelkraut <eichelkrautadam@yahoo.com>
2014-09-09 11:40:15 -0500

    Fixed Makefiles to properly substituted object directory path Also added skeleton for Accumulator class

---
+++ include/Accumulator.hh
@@ -0,0 +1,32 @@
+#ifndef ACCUMULATOR_HH
+#define ACCUMULATOR_HH
+/*
+ * 
+ *      Accumulator.hh
+ * 
+ *      Contributors:   Adam Eichelkraut
+ *                      Jared Brown
+ *      Contact:        ake0005@tigermail.auburn.edu
+ *                      jab0079@tigermail.auburn.edu
+ * 
+ *      Description: 
+ *          This class defines the interface for the Accumulator Simulator
+ * 
+ *      Change Log:
+ *          9/9/14 - Initial creation
+ * 
+ * 
+ */
+
+#include<iostream>
+
+class Accumulator
+{
+    public:
+        Accumulator();
+    protected:
+    private:
+};
+
+
+#endif
---
+++ src/accumCode/Accumulator.cc
@@ -0,0 +1,20 @@
+/*
+ * 
+ *      Accumulator.cc
+ * 
+ *      Contributors:   Adam Eichelkraut
+ *                      Jared Brown
+ *      Contact:        ake0005@tigermail.auburn.edu
+ *                      jab0079@tigermail.auburn.edu
+ * 
+ *      Description: 
+ *          This source file defines the implementation for the Accumulator Simulator
+ * 
+ *      Change Log:
+ *          9/9/14 - Initial creation
+ * 
+ * 
+ */
+#include "Accumulator.hh"
+
+Accumulator::Accumulator() {}
