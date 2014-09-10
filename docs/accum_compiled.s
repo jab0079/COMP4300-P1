bf7b06e3505968017c4db167b95865278bfcdab3
eichelkrauta <eichelkrautadam@yahoo.com>
2014-09-10 08:21:41 -0500

    Corrected addresses

--- docs/accum_compiled.s
+++ docs/accum_compiled.s
@@ -4,16 +4,16 @@
 0x00200000:3  #X
 0x00200004:7  #A
 0x00200008:5  #B
-0x002000012:4 #C
+0x0020000C:4 #C
 
 .text
   LOAD 0x00200000   # Load X into accumulator's register
   MULT 0x00200000   # Multiply by X (X^2)
   MULT 0x00200004   # Multiply register by A (AX^2)
-  STO 0x002000016   # Store AX^2 in address after C
+  STO 0x0020000F   # Store AX^2 in address after C
   LOAD 0x00200000   # Load X back into register
   MULT 0x00200008   # Multiply register by B (BX)
-  ADD 0x002000016   # Add (AX^2) to register
+  ADD 0x0020000F   # Add (AX^2) to register
                     # Register now contains AX^2+BX.
-  ADD 0x002000012   # AX^2+BX+C
-  STO 0x002000016   # Store output
+  ADD 0x0020000C   # AX^2+BX+C
+  STO 0x0020000F   # Store output
