9b924a64aa7cc53381089df1afe9b1231a1974b4
eichelkrauta <eichelkrautadam@yahoo.com>
2014-09-10 07:57:19 -0500

    Changed offset syntax

--- docs/accum_compiled.s
+++ docs/accum_compiled.s
@@ -10,10 +10,10 @@ C:	.word 4
   LOAD X    # Load X into accumulator's register
   MULT X    # Multiply by X (X^2)
   MULT A    # Multiply register by A (AX^2)
-  STO C+4   # Store AX^2 in address after C
+  STO 4(C)   # Store AX^2 in address after C
   LOAD X    # Load X back into register
   MULT B    # Multiply register by B (BX)
-  ADD C+4   # Add (AX^2) to register
+  ADD 4(C)   # Add (AX^2) to register
             # Register now contains AX^2+BX.
   ADD C     # AX^2+BX+C
-  STO C+4   # Store output
+  STO 4(C)   # Store output
