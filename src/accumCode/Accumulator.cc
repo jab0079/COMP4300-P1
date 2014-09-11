759e617ef762787de4f5aa1486ddff973342d1ac
jab0079 <jab0079@auburn.edu>
2014-09-11 13:16:42 -0500

    Implemented run methods in both simulators. Bug results in segmentation fault when running.

--- src/accumCode/Accumulator.cc
+++ src/accumCode/Accumulator.cc
@@ -30,7 +30,52 @@ void Accumulator::run()
 {
     std::cout << "\tStarting Accumulator..." << std::endl;
     
-//     inst curr_inst = *((inst*)m_memory->read(m_pc,sizeof(inst))); 
+    inst curr_inst = 0, opcode = 0;
+    addr address = 0x0;
+    u_int32_t value = 0;
+    
+    bool user_mode = true;
+    while (user_mode) 
+    {
+      // Read memory at PC  
+      curr_inst = *((inst*)m_memory->read(m_pc,sizeof(inst)));	// *** Causes a Segmentation Fault (core dumped) error when running
+      
+      // Get opcode and address
+      opcode = curr_inst & 0xFF000000;
+      opcode >> 24; 	// Bitwise shift opcode down
+      address = curr_inst & 0x00FFFFFF;
+      
+      // Increment PC
+      m_pc += sizeof(inst);
+      
+      switch (opcode)
+      {
+	case LOAD:
+	  m_register = *((reg*)m_memory->read(address, sizeof(reg)));	// read contents of memory at address into accumulator
+	  break;
+	  
+	case STO:
+	  m_memory->write(address, (void*)m_register, sizeof(reg));	// write value of accumulator to memory at address
+	  break;
+	  
+	case ADD:
+	  value = *((u_int32_t*)m_memory->read(address, sizeof(u_int32_t)));	// read contents of memory at address
+	  m_register += value;							// add value to accumulator
+	  break;
+	  
+	case MULT:
+	  value = *((u_int32_t*)m_memory->read(address, sizeof(u_int32_t)));	// read contents of memory at address
+	  m_register *= value;							// multiply the value with the accumulator 
+	  break;
+	  
+	case END:
+	  user_mode = false;
+	  break;
+	  
+	default:
+	  break;
+      }
+    }
     
     std::cout << "\tEnding Accumulator..." << std::endl;
 }
--- src/stackCode/Stack.cc
+++ src/stackCode/Stack.cc
@@ -10,6 +10,7 @@
  *          This class defines the implementation for the stack-based simulator.
  *
  *      Change Log:
+ * 	    9/10/14 - Finished implementation
  *          9/8/14 - Initial implementation created
  *
  *
@@ -27,8 +28,63 @@ Stack::~Stack()
 void Stack::run()
 {
     std::cout << "\tStarting Stack..." << std::endl;
-
-    inst curr_inst = (inst)m_memory->read(m_pc,sizeof(inst));
-
+    
+    inst curr_inst = 0, opcode = 0;
+    addr address = 0x0;
+    u_int32_t value1 = 0, value2 = 0, result = 0;
+    
+    bool user_mode = true;
+    while (user_mode) 
+    {
+      // Read memory at PC  
+      curr_inst = *((inst*)m_memory->read(m_pc,sizeof(inst))); 	// *** Causes a Segmentation Fault (core dumped) error when running 
+      
+      // Get opcode and address
+      opcode = curr_inst & 0xFF000000;
+      opcode >> 24; 	// Bitwise shift opcode down
+      address = curr_inst & 0x00FFFFFF;
+      
+      // Increment PC
+      m_pc += sizeof(inst);
+      
+      switch (opcode)
+      {
+	case PUSH:
+	  value1 = *((u_int32_t*)m_memory->read(address, sizeof(u_int32_t)));	// read contents of memory at address
+	  m_memory->write(m_sp, (void*)value1, sizeof(u_int32_t));		// write value onto top of stack
+	  m_sp += sizeof(u_int32_t);						// increment stack pointer
+	  break;
+	  
+	case POP:
+	  value1 = *((u_int32_t*)m_memory->read(m_sp, sizeof(u_int32_t)));	// read one word off the top of the stack
+	  m_sp -= sizeof(u_int32_t);						// decrement stack pointer
+	  m_memory->write(address, (void*)value1, sizeof(u_int32_t));		// write value to memory at address
+	  break;
+	  
+	case ADD:
+	  value1 = *((u_int32_t*)m_memory->read(m_sp, sizeof(u_int32_t)));	// read first word off the top of the stack
+	  m_sp -= sizeof(u_int32_t);						// decrement stack pointer
+	  value2 = *((u_int32_t*)m_memory->read(m_sp, sizeof(u_int32_t)));	// read second word off the top of the stack
+	  result = value1 + value2;
+	  m_memory->write(m_sp, (void*)result, sizeof(u_int32_t));		// write value to memory at address 
+	  break;
+	  
+	case MULT:
+	  value1 = *((u_int32_t*)m_memory->read(m_sp, sizeof(u_int32_t)));	// read first word off the top of the stack
+	  m_sp -= sizeof(u_int32_t);						// decrement stack pointer
+	  value2 = *((u_int32_t*)m_memory->read(m_sp, sizeof(u_int32_t)));	// read second word off the top of the stack
+	  result = value1 * value2;
+	  m_memory->write(m_sp, (void*)result, sizeof(u_int32_t));		// write value to memory at address 
+	  break;
+	  
+	case END:
+	  user_mode = false;
+	  break;
+	  
+	default:
+	  break;
+      }
+    }
+    
     std::cout << "\tEnding Stack..." << std::endl;
 }
