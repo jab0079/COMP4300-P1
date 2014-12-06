91bd8e158c65508f99cdc933982cc90154a68963
jab0079 <jab0079@auburn.edu>
2014-12-06 14:51:09 -0600

    Fixed Scoreboard propagate FU_status to work with a single FU.

--- include/Scoreboard.hh
+++ include/Scoreboard.hh
@@ -73,7 +73,7 @@ class Scoreboard
         virtual void update_fu_status_flags(const FU_ID& fu_id);
         virtual void reset_fu_status(const FU_ID& fu_id);
         
-        virtual void propagate_piped_fu_status();
+        virtual void propagate_piped_fu_status(const FU_ID& fu_id);
         
         // Gets and Sets-------------------------------------------------------
         virtual void set_instr_status(const u_int32_t& id, const SCO_CYCLE& new_status, const int32_t& cycle);
